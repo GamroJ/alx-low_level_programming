7d2afced4076e28e42cb918c4b99f87601e17e20
Sfiso Nkonyane <gamroji@gmail.com>
2023-07-20 12:19:06 -0700

    created 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry Point
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
